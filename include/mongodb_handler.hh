b1254d923c22993476164101687ac7fbdc4f17e4
Yusuf Can Anar <yusufcan.anar@tai.com.tr>
2024-02-13 18:30:11 +0300

    initial commit

---
+++ include/mongodb_handler.hh
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <unordered_map>
+
+// "/usr/local/include/bsoncxx/v_noabi" include this to  include path
+#include "bsoncxx/builder/stream/document.hpp"
+#include "bsoncxx/json.hpp"
+#include "bsoncxx/oid.hpp"
+#include "mongocxx/client.hpp"
+#include "mongocxx/database.hpp"
+#include "mongocxx/uri.hpp"
+
+namespace learning
+{
+    class MongoDbHandler
+    {
+// TODO This class might be deleted and functions here be used in DRMongo class. Decide it later.
+    public:
+        MongoDbHandler(const std::string mongoDbUri, const std::string databaseName, const std::string collectionName)
+            : uri(mongocxx::uri(mongoDbUri)),
+              client(mongocxx::client(uri)),
+              db(client[databaseName]),
+          _collectionName(collectionName) {}
+                
+        bool add(bsoncxx::builder::stream::document& builder) {
+            mongocxx::collection collection = db[_collectionName];
+            bsoncxx::document::value doc = builder << bsoncxx::builder::stream::finalize;
+            collection.insert_one(doc.view());
+            return true;
+        }
+
+    private:
+        mongocxx::uri uri;
+        mongocxx::client client;
+        mongocxx::database db;
+        std::string _collectionName;
+    };
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,50 @@
 
 #include "mongocxx/instance.hpp"
 
-#include "mongodb_handler.h"
-#include "character_size.h"
+#include "mongodb_handler.hh"
+#include <regex>
+void addField(bsoncxx::builder::stream::document& doc, std::string key, std::string value);
 
 int main() {
-
+    bool result = 0;
     mongocxx::instance instance;
-    learning::MongoDbHandler mongoDbHandler;
+    learning::MongoDbHandler mongoDbHandler("mongodb://10.41.74.77:27017", "learning_mongocxx", "string2json"); //TODO move this to the constructor later
+    bsoncxx::builder::stream::document doc;
+    addField(doc, "sys.exec.out.time", "0.65");
+    addField(doc, "sys.exec.out.time.cannon.pos[0]", "98");
+    addField(doc, "sys.exec.out.time.cannon.pos[1]", "34");
+    
 
-    mongoDbHandler.AddCharacterToDb("Donkey Kong", learning::CharacterSize::Large, 0);
+    // Print the BSON document
+    bsoncxx::document::value mydoc = doc << bsoncxx::builder::stream::finalize;
+    
+    std::cout << "----------------------------------------------------------------------"<< std::endl;
+    std::cout << bsoncxx::to_json(mydoc.view()) << std::endl;
 
-    std::cout << "Hello from Fenix!" << std::endl;
+    // result = mongoDbHandler.add(doc);
+
+    std::cout << "Hello from Fenix! operation is " << result << std::endl;
+}
+
+void addField(bsoncxx::builder::stream::document& doc, std::string key, std::string value) {
+    // Check if key contains dot notation
+    size_t dotPos = key.find('.');
+    size_t bracketPos = key.find('[');
+
+    if (dotPos != std::string::npos) {
+        // Handle nested fields
+        std::string nestedKey = key.substr(0, dotPos);
+        bsoncxx::builder::stream::document nestedDoc;
+        addField(nestedDoc, key.substr(dotPos + 1), value); //recursive call
+        doc << nestedKey << nestedDoc;
+    } else if (bracketPos != std::string::npos) {
+        // Handle array fields
+        std::string arrayKey = key.substr(0, bracketPos);
+        int arrayIndex = std::stoi(key.substr(bracketPos + 1, key.length() - bracketPos - 2));
+        doc << arrayKey << bsoncxx::builder::stream::open_array << value << bsoncxx::builder::stream::close_array;
+    } else {
+        // Add simple field
+        doc << key << value;
+    }
+    std::cout << bsoncxx::to_json(doc) << std::endl;
 }
